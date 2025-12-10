2e6923de2cfb78f32f959476795c86fc70b43a9a
antoniopurificato <purificato.2019135@studenti.uniroma1.it>
2025-12-10 15:31:03 +0100

    Small fix

---
+++ data/setup.sh
@@ -0,0 +1,13 @@
+
+mkdir data/Hertziana
+mv -r data/gemalde data/Hertziana/gemalde
+mv -r data/zeichnungen data/Hertziana/zeichnungen
+
+mv data/triplets_hertziana_train.json data/Hertziana/triplets_hertziana_train.json
+mv data/triplets_hertziana_val.json data/Hertziana/triplets_hertziana_val.json
+mv data/triplets_hertziana_test.json data/Hertziana/triplets_hertziana_test.json
+
+mv data/triplets_semart_train.json data/SemArt/triplets_semart_train.json
+mv data/triplets_semart_val.json data/SemArt/triplets_semart_val.json
+mv data/triplets_semart_test.json data/SemArt/triplets_semart_test.json
+mv data/triplets_semart_test_old.json data/SemArt/triplets_semart_test_old.json
