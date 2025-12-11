f071669d369e878a7c28e13d46891bffca88b7a0
antoniopurificato <purificato.2019135@studenti.uniroma1.it>
2025-12-11 08:09:27 +0100

    Small fix.

--- data/setup.sh
+++ data/setup.sh
@@ -3,11 +3,11 @@ mkdir data/Hertziana
 mv data/gemalde data/Hertziana/gemalde
 mv data/zeichnungen data/Hertziana/zeichnungen
 
-mv data/triplets_hertziana_train.json data/Hertziana/triplets_hertziana_train.json
-mv data/triplets_hertziana_val.json data/Hertziana/triplets_hertziana_val.json
-mv data/triplets_hertziana_test.json data/Hertziana/triplets_hertziana_test.json
+cp data/triplets_hertziana_train.json data/Hertziana/triplets_hertziana_train.json
+cp data/triplets_hertziana_val.json data/Hertziana/triplets_hertziana_val.json
+cp data/triplets_hertziana_test.json data/Hertziana/triplets_hertziana_test.json
 
-mv data/triplets_semart_train.json data/SemArt/triplets_semart_train.json
-mv data/triplets_semart_val.json data/SemArt/triplets_semart_val.json
-mv data/triplets_semart_test.json data/SemArt/triplets_semart_test.json
-mv data/triplets_semart_test_old.json data/SemArt/triplets_semart_test_old.json
+cp data/triplets_semart_train.json data/SemArt/triplets_semart_train.json
+cp data/triplets_semart_val.json data/SemArt/triplets_semart_val.json
+cp data/triplets_semart_test.json data/SemArt/triplets_semart_test.json
+cp data/triplets_semart_test_old.json data/SemArt/triplets_semart_test_old.json
